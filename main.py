cbeba2c04ea89e8e6a0008ce79bc574373999be0
Hanna Arhipova <harhipova@mirantis.com>
2019-08-29 18:35:19 +0300

    Add examples with complex numbers

--- main.py
+++ main.py
@@ -13,5 +13,15 @@ if __name__ == '__main__':
     logging.info(calc.Subtract(-10, b=6))
     logging.info(calc.Multiply(a=6, b=5))
     logging.info(calc.Multiply(-16, 0.00005))
-    logging.info(calc.Sum(complex(1, 2), complex(1, 2)))
 
+    logging.info(calc.Sum(complex(1, 2), complex(1, 2)))
+    logging.info(calc.Divide(complex(1, 2), complex(1, 2)))
+    logging.info(calc.Multiply(complex(5, 3), complex(1, 2)))
+    logging.info(calc.Subtract(complex(7, 6), complex(1, 2)))
+
+    logging.info(calc.Subtract(complex(1, 2), 2))
+
+    try:
+        logging.info(calc.Divide('110', '10'))
+    except BaseException as e:
+        print("Can't do operation with binary numbers")
