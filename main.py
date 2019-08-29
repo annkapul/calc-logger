e9beed4bf190ed87beef652041b9e692a476bce6
Hanna Arhipova <harhipova@mirantis.com>
2019-08-29 18:28:39 +0300

    Add new examples

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 if __name__ == '__main__':
-    logging.info(Calculator().Divide(4, 2))
-    logging.info(Calculator().Sum(3.5, 6.7))
-    logging.info(Calculator().Subtract(10, b=6))
-    logging.info(Calculator().Multiply(a=6, b=5))
+    calc = Calculator()
+    logging.info(calc.Divide(4, 2))
+    logging.info(calc.Divide(10, 3))
+    logging.info(calc.Sum(3.5, 6.7))
+    logging.info(calc.Sum('aaaa', 'BB'))
+    logging.info(calc.Subtract(10, b=6))
+    logging.info(calc.Subtract(-10, b=6))
+    logging.info(calc.Multiply(a=6, b=5))
+    logging.info(calc.Multiply(-16, 0.00005))
+    logging.info(calc.Sum(complex(1, 2), complex(1, 2)))
+
